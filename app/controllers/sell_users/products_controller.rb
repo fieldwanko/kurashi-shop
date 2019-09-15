5ddb5602cbda0f3ccd28f6507b011376d9431148
fieldwanko <field.kou815@gmail.com>
2019-09-15 10:53:55 +0000

    [finished]search product / add product stock

--- app/controllers/sell_users/products_controller.rb
+++ app/controllers/sell_users/products_controller.rb
@@ -1,6 +1,9 @@
 class SellUsers::ProductsController < ApplicationController
   def index
-    @products = Product.where(sell_user_id: current_sell_user.id)
+    @my_products = Product.where(sell_user_id: current_sell_user.id)
+    @products = @my_products.order(created_at: :desc).page(params[:page]).per(20)
+    @search = @products.ransack(params[:q])
+    @result = @search.result
   end
 
   def show
@@ -36,6 +39,10 @@ class SellUsers::ProductsController < ApplicationController
   private
     def product_params
       params.require(:product).permit(:title,:price,:content,product_images_product_images: [],product_arrival_attributes: [:stock])
+    end
+
+    def search_params
+      params.require(:q).permit!
     end
 
 end
